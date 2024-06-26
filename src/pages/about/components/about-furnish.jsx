678ad4c19a50139b657586f0519ac408b0c8cf29
AJ-Appiah <ericapph@gmail.com>
2024-06-26 16:53:49 +0000

    About page beginning

---
+++ src/pages/about/components/about-furnish.jsx
@@ -0,0 +1,7 @@
+const AboutFurnish = () => {
+  return (
+    <div>AboutFurnish</div>
+  )
+}
+
+export default AboutFurnish
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,15 @@
+import Navbar from "../../components/navbar"
+import AboutFurnish from "./components/about-furnish"
+import Hero from "./components/about-hero"
+
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <Navbar/>
+        <AboutFurnish/>
+        
+
+    </div>
   )
 }
 
