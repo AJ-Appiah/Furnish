d8f83b3e7dc0be840f5424bb772b17021a260ef5
AJ-Appiah <ericapph@gmail.com>
2024-06-26 16:25:58 +0000

    Furnish App first update

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,32 @@
+import Navbar from "../../components/navbar"
+import About from "./components/about"
+import Blog from "./components/blog"
+import Collection from "./components/collection"
+import Footer from "./components/footer"
+import Hero from "./components/hero"
+import Services from "./components/services"
+import Statistics from "./components/statistics"
+import Testimonials from "./components/testimonials"
+import ProductionFlows from "./production-flows"
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+        <Services/>
+        <About/>
+        <Collection/>
+        <Statistics/>
+        <Testimonials/>
+        <ProductionFlows/>
+        <Blog/>
+        <Footer/>
+        
+        
+    
+    </div>
+  )
+}
+
+export default LandingPage 
